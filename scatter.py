93a01839fd1e0e2652b0a7fdf6ac5fe741ca5329
Leva <novikovlev48@gmail.com>
2026-03-29 19:42:30 +0200

    added clarity comments to scatter function

--- scatter.py
+++ scatter.py
@@ -4,15 +4,17 @@ from irisClass import Iris
 
 # plots a scatter graph using following parameters as x and y axes
 
-# 0 is 'Sepal length'
-# 1 is 'Sepal width'
-# 2 is 'Petal length'
-# 3 is 'Petal width'
+#   0 is 'Sepal length'
+#   1 is 'Sepal width'
+#   2 is 'Petal length'
+#   3 is 'Petal width'
+
+# so for example, if parameters = (0, 1)
+#   we compare Sepal length and Sepal width
 
 def plotScatter(setosas : Iris, versicolors : Iris, virginicas : Iris, parameters : tuple):
 
-    print([row[parameters[0]] for row in setosas.data])
-
+    # retrieving data from func-variables
     setosasX = [float(row[parameters[0]]) for row in setosas.data]
     setosasY = [float(row[parameters[1]]) for row in setosas.data]
     versicolorsX = [float(row[parameters[0]]) for row in versicolors.data]
@@ -20,6 +22,8 @@ def plotScatter(setosas : Iris, versicolors : Iris, virginicas : Iris, parameter
     virginicasX = [float(row[parameters[0]]) for row in virginicas.data]
     virginicasY = [float(row[parameters[1]]) for row in virginicas.data]
 
+
+    # plotting graph
     plt.scatter(setosasX, setosasY, c = 'blue', alpha = 0.3,
                 label = 'Setosas')
     plt.scatter(versicolorsX, versicolorsY, c = 'red', alpha = 0.3,
@@ -28,8 +32,10 @@ def plotScatter(setosas : Iris, versicolors : Iris, virginicas : Iris, parameter
                 label = 'Virginicas')
 
 
+    #decorating
     attributes = ['Sepal length','Sepal width','Petal length','Petal width']
     title = 'Relationship between ' + attributes[parameters[0]] + ' and ' + attributes[parameters[1]]
+
     plt.title(title, fontweight = 'bold')
     plt.xlabel(attributes[parameters[0]], fontweight = 'bold')
     plt.ylabel(attributes[parameters[1]], fontweight = 'bold')
